9aef10d98d5bd2cde7a30ac9a97d51bf0c44b8bd
Koen Rijpstra <k.rijpstra@gmail.com>
2024-09-05 09:40:06 +0200

    Load DB in web worker

--- packages/airdrop-sender/src/workers/worker.ts
+++ packages/airdrop-sender/src/workers/worker.ts
@@ -1,5 +1,17 @@
-addEventListener("message", (e) => {
-  if (e.data === "hello") {
-    postMessage("hiya!");
-  }
+import { loadDB } from "../services/db";
+import { transaction_queue } from "../schema/transaction_queue";
+import { maxAddressesPerTransaction } from "../config/constants";
+import { getTableName, sql } from "drizzle-orm";
+
+addEventListener("message", async (e) => {
+  const db = await loadDB();
+  // Create will overwrite any existing airdrop
+  // Delete the existing airdrop queue
+  // Delete the sqlite_sequence record to reset the autoincrement
+  await db.delete(transaction_queue);
+  await db.run(
+    sql`DELETE FROM sqlite_sequence WHERE name = ${getTableName(transaction_queue)}`
+  );
+
+  postMessage("done!" + maxAddressesPerTransaction);
 });
