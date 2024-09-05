ea42d809cad3d1461b025b64524e12c52608f7cf
Koen Rijpstra <k.rijpstra@gmail.com>
2024-09-05 12:19:28 +0200

    Disable swcMinify to fix build error

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -8,6 +8,7 @@ module.exports = {
   eslint: {
     ignoreDuringBuilds: true,
   },
+  swcMinify: false,
   webpack: (config, { isServer }) => {
     if (!isServer) {
       config.resolve.fallback.fs = false;
