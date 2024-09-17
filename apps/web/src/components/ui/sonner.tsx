761ac05e6fd53e408e196002e57421928ab04f82
Koen Rijpstra <k.rijpstra@gmail.com>
2024-09-17 15:02:57 +0200

    Fix build error

--- apps/web/src/components/ui/sonner.tsx
+++ apps/web/src/components/ui/sonner.tsx
@@ -1,4 +1,4 @@
-import { useTheme } from "next-themes"
+import { useTheme } from "@/providers/ThemeProvider"
 import { Toaster as Sonner } from "sonner"
 
 type ToasterProps = React.ComponentProps<typeof Sonner>
