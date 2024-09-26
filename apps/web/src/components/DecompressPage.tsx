9f94205c0f1a7580713bd04370fc1acf02a5baf7
Koen Rijpstra <k.rijpstra@gmail.com>
2024-09-26 19:56:24 +0200

    Shorten Mint address

--- apps/web/src/components/DecompressPage.tsx
+++ apps/web/src/components/DecompressPage.tsx
@@ -227,7 +227,7 @@ export function DecompressPage() {
                   <TableBody>
                     {compressedTokenAccounts.map((account, index) => (
                       <TableRow key={index}>
-                        <TableCell>{account.parsed.mint.toBase58()}</TableCell>
+                        <TableCell>{account.parsed.mint.toBase58().slice(0, 4) + '...' + account.parsed.mint.toBase58().slice(-4)}</TableCell>
                         <TableCell>{normalizeTokenAmount(account.parsed.amount.toString(), 9)}</TableCell>
                         <TableCell>
                           <Button onClick={() => handleDecompress(account)}>Decompress</Button>
